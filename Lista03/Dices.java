4945acff301dc9b2bb68f71a4f3e628f33614283
diegocba9 <118191730+diegocba9@users.noreply.github.com>
2022-11-14 13:25:25 -0300

    Add files via upload

---
+++ Lista03/Dices.java
@@ -0,0 +1,57 @@
+package listaCampa;
+
+import java.util.Random;
+
+public class Dices {
+	private int matrix [][] = new int [7][7];
+	
+	public Dices () {
+	}
+	
+	public void print(int times) {
+		StringBuilder sb = new StringBuilder();
+		Random rand = new Random();
+		int dice1;
+		int dice2;
+		
+		for (int i=0; i<7; i++) {
+			for (int j=0; j<7; j++) {
+				if (i==0) {
+					if (j==0) {
+						matrix[i][j] = 0;
+					}
+					else {
+						matrix[i][j] = j;
+					}
+				}
+				else if(j==0) {
+					matrix[i][j] = i;
+				}
+				else {
+					matrix[i][j] = 0;
+				}
+			}		
+		}
+		
+		for (int i=0; i<times; i++) {
+			dice1 = rand.nextInt(6)+1;
+			dice2 = rand.nextInt(6)+1;		
+			
+			matrix[dice1][dice2]++;			
+		}
+		
+		for (int i=0; i<7; i++) {
+			for (int j=0; j<7; j++) {
+				if (i==0) {
+					sb.append(matrix[i][j] + "   ");
+				}
+				else {
+					sb.append(matrix[i][j] + " ");
+				}
+			}		
+			sb.append("\n");
+		}
+		
+		System.out.println(sb.toString());
+	}
+}
---
+++ Lista03/Main.java
@@ -0,0 +1,9 @@
+package listaCampa;
+
+public class Main {
+	public static void main(String[] args) {
+		Dices dice = new Dices();
+		
+		dice.print(36000);
+	}
+}
