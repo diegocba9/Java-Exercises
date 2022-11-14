c3598445427ab517e813245cef71760ae4317492
diegocba9 <118191730+diegocba9@users.noreply.github.com>
2022-11-14 12:20:32 -0300

    Add files via upload

---
+++ Básico-Java/Carro.java
@@ -0,0 +1,84 @@
+package ListaEx;
+
+public class Carro {
+	private String placa;
+	private int ano;
+	private String modelo;
+	private double velocidade;
+	private static int numCarros;
+	
+	public Carro (String placa, int ano, String modelo) {
+		this.placa = placa;
+		this.ano = ano;
+		this.modelo = modelo;
+		this.velocidade = 0;
+		numCarros++;
+	}
+	
+	public String getPlaca() {
+		return placa;
+	}
+	
+	public void setPlaca(String placa) {
+		this.placa = placa;
+	}
+	
+	public int getAno() {
+		return ano;
+	}
+	
+	public void setAno(int ano) {
+		this.ano = ano;
+	}
+	
+	public String getModelo() {
+		return modelo;
+	}
+	
+	public void setModelo(String modelo) {
+		this.modelo = modelo;
+	}
+	
+	public double getVelocidade() {
+		return velocidade;
+	}
+	
+	public void setVelocidade(double velocidade) {
+		this.velocidade = velocidade;
+	}
+	
+	public int getNumCarros () {
+		return numCarros;
+	}
+	
+	public void mostraCarro (Carro carro) {
+		
+		System.out.print("\nPlaca do carro: ");
+		System.out.println(carro.getPlaca());
+		
+		System.out.print("Ano do carro: ");
+		System.out.println(carro.getAno());
+		
+		System.out.print("Modelo do carro: ");
+		System.out.println(carro.getModelo());
+		
+		System.out.print("Velocidade do carro: ");
+		System.out.println(carro.getVelocidade());
+	}
+	
+	public double acelerar (Carro carro) {
+		double velocidade;
+		velocidade = carro.getVelocidade();
+		velocidade ++;
+		carro.setVelocidade(velocidade);
+		return velocidade;
+	}
+	
+	public double frear (Carro carro) {
+		double velocidade;
+		velocidade = carro.getVelocidade();
+		velocidade --;
+		carro.setVelocidade(velocidade);
+		return velocidade;
+	}
+}
---
+++ Básico-Java/TesteCarro.java
@@ -0,0 +1,120 @@
+package ListaEx;
+
+import java.util.Scanner;
+import java.util.ArrayList;
+
+public class TesteCarro {
+	public static void main(String[] args) {
+
+		Scanner input = new Scanner(System.in);
+		int option;
+
+		ArrayList<Carro> listaCarros = new ArrayList<Carro>();
+
+		while (true) {
+
+			int count = -1;
+
+			System.out.println("1 - Cadastrar carro");
+			System.out.println("2 - Imprimir dados");
+			System.out.println("3 - Testar Aceleração");
+			System.out.println("4 - Testar freios");
+			System.out.println("5 - Encerrar");
+			option = input.nextInt();
+
+			switch (option) {
+			case 1:
+
+				String placa;
+				System.out.print("Insira a placa do carro: ");
+				placa = input.next();
+
+				int ano;
+				System.out.print("Insira o ano do carro: ");
+				ano = input.nextInt();
+
+				String modelo;
+				System.out.print("Insira o modelo do carro: ");
+				modelo = input.next();
+
+				Carro carro1 = new Carro(placa, ano, modelo);
+				listaCarros.add(carro1);
+
+				break;
+
+			case 2:
+
+				while (count == -1) {
+					System.out.print("Insira qual carro você quer imprimir: ");
+					count = input.nextInt();
+
+					if (listaCarros.isEmpty() == true) {
+						System.out.println("Nenhum carro registrado ainda!");
+					}
+
+					else if (count < 1 || count > listaCarros.get(0).getNumCarros()) {
+						System.out.println("Insira um carro válido!");
+						count = -1;
+					}
+
+					else {
+						listaCarros.get(count - 1).mostraCarro(listaCarros.get(count - 1));
+					}
+				}
+
+				break;
+
+			case 3:
+
+				while (count == -1) {
+					System.out.print("Insira qual carro você quer testar a aceleração: ");
+					count = input.nextInt();
+
+					if (listaCarros.isEmpty() == true) {
+						System.out.println("Nenhum carro registrado ainda!");
+					}
+
+					else if (count < 1 || count > listaCarros.get(0).getNumCarros()) {
+						System.out.println("Insira um carro válido!");
+						count = -1;
+					}
+
+					else {
+						listaCarros.get(count - 1).acelerar(listaCarros.get(count - 1));
+						System.out.println("Velocidade: " + listaCarros.get(count - 1).getVelocidade());
+					}
+				}
+
+				break;
+
+			case 4:
+				while (count == -1) {
+					System.out.print("Insira qual carro você quer testar a aceleração: ");
+					count = input.nextInt();
+
+					if (listaCarros.isEmpty() == true) {
+						System.out.println("Nenhum carro registrado ainda!");
+					}
+
+					else if (count < 1 || count > listaCarros.get(0).getNumCarros()) {
+						System.out.println("Insira um carro válido!");
+						count = -1;
+					}
+
+					else {
+						listaCarros.get(count - 1).frear(listaCarros.get(count - 1));
+						System.out.println("Velocidade: " + listaCarros.get(count - 1).getVelocidade());
+					}
+
+				}
+
+				break;
+
+			case 5:
+				System.exit(1);
+
+			}
+
+		}
+	}
+}
